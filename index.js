ce60774e1ea1eaa4cccc2e36d99c6d123327aa9f
Anthony Guirges <anthony.g.guirges@gmail.com>
2022-07-20 15:21:45 -0700

    added range input and operator selections

--- index.js
+++ index.js
@@ -1,7 +1,83 @@
+//make sure DOM is loaded before program execution
 $(document).ready(function() {
-    var currentQuestion;
+//global variables
+var interval;
+var currentQuestion;
+var timeLeft = 10;
+var score = 0;
+var add = document.getElementById('add');
+var subtract = document.getElementById('subtract');
+var multiply = document.getElementById('multiply');
+var divide = document.getElementById('divide');
+var operators;
+var slider = $('#limit');
+var sliderDisplay = $('#number-text');
+sliderDisplay.html(slider.val());
+
+//update amount of time
+var updateTimeLeft = function(amount) {
+    timeLeft += amount;
+    $('#time-left').text(timeLeft);
+  }
+//display slider value
+  slider.on('input', function() {
+    sliderDisplay.html($(this).val());
+  })
+
+  //update score
+  var updateScore = function(amount) {
+    score += amount;
+    $('#score').text(score);
+  }
+
+//set interval that counts down a timer of 10 sec and starts game
+var startGame = function () {
+    if (!interval) {
+        if (timeLeft === 0) {
+            updateTimeLeft(10);
+            updateScore(-score);
+        }
+      interval = setInterval(function () {
+        updateTimeLeft(-1);
+        if (timeLeft === 0) {
+          clearInterval(interval);
+          interval = undefined;
+        }
+      }, 1000);
+    }
+  }
+
+//select operator of choice +, -, *, /
+  var selectOperator = function() {
+    var operator = [];
+
+    if(add.checked === true) {
+        operator.push('+');
+    }
+    if (subtract.checked === true) {
+        operator.push('-');
+    }
+    if (multiply.checked === true) {
+        operator.push('*');
+    }
+    if (divide.checked === true) {
+        operator.push('/');
+    }
+    if (operator.length === 0) {
+        operators = '+';
+    }
+    else if (operator.length === 1) {
+        operators = operator[0];
+    }
+    else {
+        var i = Math.floor(Math.random() * operator.length);
+        operators = operator[i];
+    }
+    return operators;
+  }
+
+//create a radom number generator using the Math.ceil and Math.random functions
 var randomNumberGenerator = function(size) {
-    //create a radom number generator using the Math.ceil and Math.random functions
     return Math.ceil(Math.random() * size);
 }
 
@@ -9,14 +85,46 @@ var questionGenerator = function() {
     //create a new object
     var question = {};
     //insert generator fuction inside two variables
-    var number1 = randomNumberGenerator(10);
-    var number2 = randomNumberGenerator(10);
-    //insert new objects within question object
-    question.answer = number1 + number2;
-    question.equation = String(number1) + ' + ' + String(number2);
+    var number1 = randomNumberGenerator(slider.val());
+    var number2 = randomNumberGenerator(slider.val());
+    var smallestNumber;
 
-    return question;
+    while(number1 === number2) {
+        number1 = randomNumberGenerator(slider.val());
+    }
+
+    if (number1 < number2) {
+        smallestNumber = number1;
+        number1 = number2;
+        number2 = smallestNumber;
+    }
+
+    var number3 = number1 * number2;
+
+    //insert new objects within question object
+    selectOperator();
+    if (operators === '+') {
+        question.answer = number1 + number2;
+        question.equation = String(number1) + operators + String(number2);
+    }
+    else if (operators === '-') {
+        question.answer = number1 - number2;
+        question.equation = String(number1) + operators + String(number2);
+    }
+    else if (operators === '*') {
+        question.answer = number1 * number2;
+        question.equation = String(number1) + operators + String(number2);
+    }
+    else if (operators === '/') {
+        question.answer = number3 / number1;
+        question.equation = String(number3) + operators + String(number1);
+    }
+    console.log(number1);
+    console.log(number2);
+    console.log(number3);
+        return question;
 }
+
     var renderNewQuestion = function() {
     //use global variable and combine it with questionGenerator function
     currentQuestion = questionGenerator();
@@ -28,13 +136,23 @@ var questionGenerator = function() {
     var checkAnswer = function(userInput, answer) {
         if (userInput === answer) {
             renderNewQuestion();
+            $('#user-input').val('');
+            updateTimeLeft(+1);
+            updateScore(+1);
         }
     }
 
-    //event listner when user inputs answer on keyup
+   $('.maths').on('click', function() {
+        selectOperator();
+        console.log('clicked');
+   })
+
+    // start game and check answer on keyup from user
     $('#user-input').on('keyup', function () {
+        startGame();
         checkAnswer(Number($(this).val()), currentQuestion.answer);
       });
 
-      renderNewQuestion();
+
+    renderNewQuestion();
 })
